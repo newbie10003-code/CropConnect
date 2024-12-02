4d1c34b217474151ec764618cff3806c972ea46f
Kajal Rawat <140612013+kajalrawat3603@users.noreply.github.com>
2024-12-02 22:08:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors({ origin: 'http://localhost:3000', methods: 'GET,POST,DELETE', credentials: true }));
+app.use(cors({ origin: 'https://crop--connect.vercel.app', methods: 'GET,POST,DELETE', credentials: true }));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
